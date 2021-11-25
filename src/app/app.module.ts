b3c2037244f8d3c29a9792386e4dfb9f055827ad
Vikram <pandey.vikram@outlook.com>
2021-11-25 16:59:50 -0600

    circularProgressComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,31 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { RoundProgressBarComponent } from '../components/round-progress-bar/round-progress-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Sensors } from '@ionic-native/sensors';
 import { Pedometer } from '@ionic-native/pedometer';
 import { BackgroundMode } from '@ionic-native/background-mode';
 import { Autostart } from '@ionic-native/autostart';
+import { RoundProgressModule } from 'angular-svg-round-progressbar';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    RoundProgressBarComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    RoundProgressModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    RoundProgressBarComponent
   ],
   providers: [
     StatusBar,
